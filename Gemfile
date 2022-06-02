3fac22a6d8d6e29f4adfac1639209975776941d9
GGG <kuranberitorappu@gmail.com>
2022-06-02 09:06:04 +0000

    ユーザーテーブル作成

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'rexml'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'letter_opener_web'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :communities
+  root 'communities#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
